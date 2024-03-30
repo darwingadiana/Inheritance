b2c1c2eb2556abaa423f1a442c091c1339180742
darwingadiana <darwingadiana@gmail.com>
2024-03-30 10:03:13 +0800

    Add output says my name

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
+        System.out.println("This is Darwin Gadiana");
 
     }
 }
